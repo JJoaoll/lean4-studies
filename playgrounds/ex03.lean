30ca03dcb5c4bbb567087f22aa46984d26fae51e
JJoaoll <joaoduos@gmail.com>
2025-04-09 18:26:27 -0300

    resync

--- playgrounds/ex03.lean
+++ playgrounds/ex03.lean
@@ -162,71 +162,74 @@ example : (p → q ∨ r) → ((p → q) ∨ (p → r)) := by
     cases hp_pₒr hp
     · apply Or.inl ; intros ; assumption
     · apply Or.inr ; intros ; assumption
-
-
-
-
+  · apply Or.inl ; intros ; contradiction
+  
 -- not and -> or not not
-example : ¬(p ∧ q) → ¬p ∨ ¬q :=
-  fun hn_pq   =>
-    let hp_np := em p
-    Or.elim hp_np
-    (fun hp  =>
-      suffices hnq : ¬q from Or.inr hnq
-      (fun hq =>
-        let hpq := And.intro hp hq
-        absurd hpq hn_pq))
-    (fun hnp => Or.inl hnp)
+example : ¬(p ∧ q) → ¬p ∨ ¬q := by 
+  intro hn_pq 
+  apply byContradiction
+  intro hn_npₒnq
+  cases em p <;> cases em q
+  · have _ : p ∧ q := by
+      constructor <;> assumption
+    contradiction 
+  all_goals have _ : ¬p ∨ ¬q := by 
+    { first | apply Or.inl ; assumption
+            | apply Or.inr ; assumption }
+
+  all_goals contradiction
 
 #check Not.imp
 -- i really dont care to much about it in a not
 -- interactive way, omaga, look at that!
 -- but if it was a easiest way, i would like to know
-example : ¬(p → q) → p ∧ ¬q :=
-  fun hn_ptoq =>
-    let hp_np := em p
-    let hq_nq := em q
-    have f : (¬q → ¬p) → (p → q) :=
-      fun hnq_np hp =>
-        Or.elim hq_nq
-        (fun hq => hq)
-        (fun hnq =>
-          absurd hp (hnq_np hnq))
-    Or.elim hq_nq
-      (fun hq => absurd (fun _ => hq) (hn_ptoq))
-      (fun hnq =>
-        Or.elim hp_np
-        (fun hp => ⟨hp, hnq⟩)
-        (fun hnp =>
-          suffices hp_q : p → q from absurd hp_q hn_ptoq
-          suffices hnq_np : ¬q → ¬p from f hnq_np
-          show ¬q → ¬p from fun _ => hnp))
+example : ¬(p → q) → p ∧ ¬q := by 
+  intros 
+  constructor <;> by_cases p <;> by_cases q
+  any_goals assumption
+  all_goals suffices hp_q : p → q by contradiction 
+  all_goals intros 
+  repeat (first | assumption | contradiction)
+
+  /- constructor <;> by_cases p <;> by_cases q -/
+  /- all_goals intros -/
+  /- any_goals (first | assumption | contradiction) -/
 
 #check byCases
-example : (p → q) → (¬p ∨ q) :=
-  fun hp_q => byCases
-    (fun hp  :  p => Or.inr $ hp_q hp)
-    (fun hnp : ¬p => Or.inl hnp)
+
+example : (p → q) → (¬p ∨ q) := by 
+  intro hp_q 
+  by_cases p 
+  · apply Or.inr
+    apply hp_q 
+    assumption
+  · apply Or.inl
+    assumption 
 
 #check byContradiction
 -- whats ur name, theorem? if i knewd would be awesome ;-;
 -- Loogle when?
-example : (¬q → ¬p) → (p → q) :=
-  fun hnq_np hp => byContradiction
-   (fun hnq =>
-     let hnp := hnq_np hnq
-     absurd hp hnp)
+example : (¬q → ¬p) → (p → q) := by 
+  intro h hp 
+  by_cases q 
+  · assumption 
+  · exfalso 
+    suffices hnp : ¬p by contradiction
+    apply h 
+    assumption 
 
-example : p ∨ ¬p := em p
-example : (((p → q) → p) → p) :=
-  fun hpk => byCases
-    (fun hp  :  p => hp)
-    (fun hnp : ¬p =>
-      suffices hp_q : p → q from hpk hp_q
-      fun hp => absurd hp hnp)
+example : p ∨ ¬p := by 
+  exact em p 
 
+example : (((p → q) → p) → p) := by 
+  intro h 
+  apply byContradiction
+  intro hnp
+  suffices hp : p by contradiction
+  apply h 
+  intros ; contradiction
 
-
+-- (i : I) ⊢ A :(i)
 -- syntax "QED" : tactic
 
 -- macro_rules
